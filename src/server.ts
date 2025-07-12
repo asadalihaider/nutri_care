2fc032fe331a02b9bab7ddb49f29f2bfd27c4cdd
Asad Ali Haider <itsasadalihaider@gmail.com>
2025-07-13 00:20:20 +0500

    minor fixes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import app from './app';
 import { config } from './config/env';
+import { startFollowupJob } from './jobs/followup.job';
 
 app.listen(config.port, () => {
   console.log(`🚀 Server running at http://localhost:${config.port}`);
+  startFollowupJob();
 });
