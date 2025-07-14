a037ea66e96fd4c1c38d14a6e7fd2116099a6498
Asad Ali Haider <itsasadalihaider@gmail.com>
2025-07-14 13:04:23 +0500

    turning the fixed string values to enums

---
+++ src/types/enums.ts
@@ -0,0 +1,12 @@
+export enum EnergyLevel {
+  LOW = 'low',
+  MODERATE = 'moderate',
+  HIGH = 'high',
+}
+
+export enum WaterIntake {
+  LESS_THAN_1L = 'Less than 1 liter',
+  ONE_TO_TWO_L = '1-2 liters',
+  TWO_TO_THREE_L = '2-3 liters',
+  MORE_THAN_3L = 'More than 3 liters',
+}
