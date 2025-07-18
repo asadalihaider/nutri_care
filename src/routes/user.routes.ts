6e6e5b5e2a51f2fe516708643ac1a4f9b0d70b91
Asad Ali Haider <itsasadalihaider@gmail.com>
2025-07-19 01:01:21 +0500

    add missing code

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import {
+  uploadImage,
   submitProfile,
   submitQuestionnaire,
   getUserData,
 } from '../controllers/user.controller';
 import { validateBody } from '../middlewares/validate.middleware';
 import { authenticate } from '../middlewares/auth.middleware';
-import { profileSchema, questionnaireSchema } from '../validators/user.validator';
+import { uploadImageSchema, profileSchema, questionnaireSchema } from '../validators/user.validator';
 
 const router = Router();
 
@@ -15,5 +16,6 @@ router.use(authenticate);
 router.get('/data', getUserData);
 router.post('/profile', validateBody(profileSchema), submitProfile);
 router.post('/questionnaire', validateBody(questionnaireSchema), submitQuestionnaire);
+router.post('/upload-image', validateBody(uploadImageSchema), uploadImage);
 
 export default router;
